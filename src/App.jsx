4ae101d3a0f779e4a7e82e965c51f75909313da7
Ingsolorzano <france.websites@gmail.com>
2023-03-12 20:41:30 +0100

    suppression de commentaires fichier App

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,10 @@ import React from "react";
 
 import MyRoutes from "./Routes";
 
-/*la fonction 'App() appelle le fichier 'MyRoutes.jsx'
-grâce à la 'balise' REACT: '<MyRoutes />')
-qui fait réference à l'import du même nom ci-dessus.
-*/
-
 function App() {
   return (
     <MyRoutes />
   );
 }
 
-/* Avec REACT, pour qu'une fonction puisse être 'importée'
-et donc utilisé dans un autre fichier, 
-il faut d'abord qu'elle soit 'exportée' ainsi:
-*/
 export default App;
